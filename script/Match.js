bc543bf99788ed6cc047b21122863fd24da6c15a
Megha Albin <megha.albin@edu.futuregames.se>
2025-01-05 22:33:01 +0100

    fix

--- script/Match.js
+++ script/Match.js
@@ -77,3 +77,5 @@ function checkMatch() {
 
 restartButton.addEventListener('click', startGame);
 startGame();
+console.log("Initializing game...");
+console.log("Shuffled card values:", shuffledValues);
